880f0c123417babb439d5fd08cd7c6a85a412fab
adewolff <30436922+adewolff@users.noreply.github.com>
2018-01-31 22:59:45 -0800

    Practice reading and writing to external .txt files

---
+++ dr_suess/green_eggs_ham.py
@@ -0,0 +1,16 @@
+'''Imports suess.txt, and turns all i into I where appropriate,
+   then writes it to a new txt file'''
+def main():
+    importer()
+
+def importer():
+    '''Imports suess.txt, and turns all i into I where appropriate,
+       then writes it to a new txt file'''
+
+    suess = open('suess.txt', 'r')
+    content = suess.read()
+    content = content.replace('i ', 'I ')
+    content = content.replace('i-', 'I-')
+    print(content, file=open('suess_fix.txt', 'w'))
+
+main()
