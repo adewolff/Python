5c20c9c6581da2485ab985ee4c8603d294064e44
adewolff <30436922+adewolff@users.noreply.github.com>
2018-02-01 22:54:56 -0800

    added linkscore function
    added linkscore function that will return karma for latest link posted.
    Planned: move to reddituser class.

--- reddit_karma/karma.py
+++ reddit_karma/karma.py
@@ -1,17 +1,36 @@
 '''Compares Karma received between 2 users' most recent posts'''
-from reddit_user import *
+from reddit_user import reddituser
 import ast
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    user1 = input("user1: ")
+    user1 = "wolfypolli"
+    user2 = "amaklp"
+
     user1data = jdata(user1)
-    print(user1data['data']['children'][0]['data']['domain'])
+    user2data = jdata(user2)
+
+    linkscore(user1data)
+    linkscore(user2data)
 
 
 def jdata(username):
     user = reddituser(username)
     profdata = user.importer()
     return profdata
+
+def linkscore(userdata):
+    '''returns the karma for the most recent link posted'''
+    i= 0
+    type = "0"
+    while type not in {"t1"}:
+        type = userdata['data']['children'][i]['kind']
+        i += 1
+        continue
+    linkkarma = userdata['data']['children'][i]['data']['score']
+    logging.debug("link karma is {}".format(linkkarma))
+    return linkkarma
 
 
 if __name__ == '__main__':
